d28665f8e3d7809d3d7e8b810fd2bb96ee90a765
Roman Tuqtarov <rtuqtarov@gmail.com>
2023-04-09 20:11:38 +0300

    коммит из pycharm в ветку master

---
+++ test_google_search.py
@@ -0,0 +1,21 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+
+
+@pytest.fixture
+def open_chrome_with_size():
+    browser.open('https://google.com/')
+    browser.driver.set_window_size(720, 600)
+
+
+def test_success_result(open_chrome_with_size):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('#search').should(have.text('Selene -'
+                                                ' User-oriented Web UI browser tests in Python'))
+    print(' тест успешно пройден - искомая фраза содержится в выдаче 1/2')
+
+def test_failed_result(open_chrome_with_size):
+    browser.element('[name="q"]').should(be.blank).type('flpagmdsgmkdspgmdsa').press_enter()
+    browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
+    print(' тест успешно пройден - по запросу нет результатов в выдаче - 2/2')
