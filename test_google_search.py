9d998e75fe32c4d208d62ca4792434ae801b04a6
Roman Tuqtarov <rtuqtarov@gmail.com>
2023-04-10 04:52:48 +0300

    коммит из pycharm в ветку master

--- test_google_search.py
+++ test_google_search.py
@@ -9,13 +9,13 @@ def open_chrome_with_size():
     browser.driver.set_window_size(720, 600)
 
 
-def test_success_result(open_chrome_with_size):
+def test_coinciding_result(open_chrome_with_size):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('Selene -'
                                                 ' User-oriented Web UI browser tests in Python'))
     print(' тест успешно пройден - искомая фраза содержится в выдаче 1/2')
 
-def test_failed_result(open_chrome_with_size):
+def test_empty_result(open_chrome_with_size):
     browser.element('[name="q"]').should(be.blank).type('flpagmdsgmkdspgmdsa').press_enter()
     browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
     print(' тест успешно пройден - по запросу нет результатов в выдаче - 2/2')
